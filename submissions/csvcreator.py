ba5333503c14315b653f241af370ee1e42862d60
Mahir Rahman <mahirrahman2000@gmail.com>
2020-11-08 14:22:40 +0000

    Jikan.py API used to import image link and MAL description to the CSVs as well as time module to rate limit requests to the API's allowed one request per 4 second per bulk requests.

--- submissions/csvcreator.py
+++ submissions/csvcreator.py
@@ -1,4 +1,7 @@
+from jikanpy import Jikan
 import csv
+import time
+jikan = Jikan()
 
 slotA = []
 slotB = []
@@ -10,31 +13,39 @@ with open('submissions/AnimeSoc Termly Anime Nomination Form (Responses) - Form 
 
     for row in readCSV:
         currentitem = 4
+        id = ''
+        for i in row[currentitem+1]:
+            if str.isdigit(i):
+                id=id+i
+            if i == '/' and len(id) > 1:
+                break
         
         while currentitem <= 16:
             if row[currentitem+2] == 'Slot A':
-                slotA.append([row[currentitem],row[currentitem+1],row[currentitem+3]])
+                slotA.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
             elif row[currentitem+2] == 'Slot B':
-                slotB.append([row[currentitem],row[currentitem+1],row[currentitem+3]])
+                slotB.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
             elif row[currentitem+2] == 'Slot C':
-                slotC.append([row[currentitem],row[currentitem+1],row[currentitem+3]])
-
+                slotC.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
+            time.sleep(4)
             currentitem+=4
 
 with open('Slot A.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Description'])
+    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
     for row in slotA:
-        filewriter.writerow([row[0],row[1],row[2]])
+        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
 
 with open('Slot B.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Description'])
+    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
     for row in slotB:
-        filewriter.writerow([row[0],row[1],row[2]])
+        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
 
 with open('Slot C.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Description'])
+    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
     for row in slotC:
-        filewriter.writerow([row[0],row[1],row[2]])
+        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
+
+print("Complete")
