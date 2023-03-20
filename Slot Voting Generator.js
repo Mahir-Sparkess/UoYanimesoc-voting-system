a53e90e0f71926acf38d47cf7f3407ed6dd44a9d
Mahir Rahman <mahirrahman2000@gmail.com>
2023-03-20 22:31:25 +0000

    Term 2 2022/23 update

--- Slot Voting Generator.js
+++ Slot Voting Generator.js
@@ -1,11 +1,10 @@
 function generateSlotForm(){
 var slotdata = SpreadsheetApp.getActiveSheet().getDataRange().getValues()
-var form = FormApp.create('AnimeSoc Slot _ Voting: Round _')
+var form = FormApp.create('AnimeSoc Slot A Voting')
 form.setConfirmationMessage("Thanks for voting on what shows to watch: https://www.youtube.com/watch?v=XmAVADh4HXk")
 form.setAllowResponseEdits(true)
 form.setLimitOneResponsePerUser(true)
 
-form.addSectionHeaderItem().setTitle("AnimeSoc Voting").setHelpText("This round 1 will consist of a seeding system and minor elimination. You will be asked to rate your desire to watch each show from -2 to 2 (-2 being low desire to watch, 0 being neutral, 2 being high desire to watch). This will be used to seed each show for the next round, which will be an elimination bracket.")
 form.addTextItem().setTitle("Name").setRequired(true)
 form.addTextItem().setTitle("Email Address").setRequired(true).setValidation(FormApp.createTextValidation().requireTextIsEmail().build())
 
@@ -13,8 +12,9 @@ for(row = 1;row != slotdata.length;row++){
   form.addPageBreakItem().setTitle(slotdata[row][0]).setHelpText(slotdata[row][1])
   form.addImageItem().setImage(UrlFetchApp.fetch(slotdata[row][3])).setAlignment(FormApp.Alignment.CENTER)
   form.addSectionHeaderItem().setTitle("Nominee Description").setHelpText(slotdata[row][2])
+  form.addVideoItem().setVideoUrl(slotdata[row][5])
   form.addSectionHeaderItem().setTitle("MAL Synopsis").setHelpText(slotdata[row][4])
-  form.addMultipleChoiceItem().setTitle("Desire to watch").setChoiceValues(['2','1','0','-1','-2']).setRequired(true)
+  form.addMultipleChoiceItem().setTitle("Desire to watch").setChoiceValues(['2','1','0','-1','-2']).setRequired(true).setHelpText('(2 = high desire to watch, 0 = neutral, -2 = low desire to watch)')
   
 }
 }
--- submissions/csvcreator.py
+++ submissions/csvcreator.py
@@ -1,50 +1,95 @@
-from jikanpy import Jikan
+from requests import request
+from urllib.parse import urlparse
 import csv
 import time
-jikan = Jikan()
+
+jikan_url = "https://api.jikan.moe/v4/"
+
+
+def get_anime(anime_id: str):
+    anime = request(method='get', url=f'{jikan_url}anime/{anime_id}/full').json()
+    return anime
+
 
 slotA = []
 slotB = []
 slotC = []
 
 
-with open('submissions/AnimeSoc Termly Anime Nomination Form (Responses) - Form responses 1.csv',newline='') as csvfile:
-    readCSV = csv.reader(csvfile,delimiter=',')
+def main():
+    with open('AnimeSoc Termly Anime Nomination Form (Responses) - Form responses 1.csv', newline='', encoding='utf-8') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        next(readCSV, None)
+        for row in readCSV:
+            row
+            currentitem = 4
 
-    for row in readCSV:
-        currentitem = 4
-        while currentitem <= 16:
-            id = ''
-            for i in row[currentitem+1]:
-                if str.isdigit(i):
-                    id=id+i
-                if i == '/' and len(id) > 1:
+            while currentitem <= 16:
+                if row[currentitem] == '':
                     break
-            if row[currentitem+2] == 'Slot A':
-                slotA.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
-            elif row[currentitem+2] == 'Slot B':
-                slotB.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
-            elif row[currentitem+2] == 'Slot C':
-                slotC.append([row[currentitem],row[currentitem+1],str(row[currentitem+3]).replace("'","\'"),jikan.anime(id).get("image_url"),jikan.anime(id).get("synopsis").replace("'","\'")])
-            time.sleep(4)
-            currentitem+=4
+                url = urlparse(row[currentitem + 1])
+                anime_id = url.path.split('/')[2]
+                anime = get_anime(anime_id)
 
-with open('Slot A.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
-    for row in slotA:
-        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
+                if row[currentitem + 2] == 'Slot A':
+                    slotA.append([
+                        row[currentitem],
+                        anime['data']['url'],
+                        str(row[currentitem + 3]).replace("'", "\'"),
+                        anime['data']['images']['jpg']['image_url'],
+                        anime['data']['synopsis'],
+                        anime['data']['trailer']['url']
+                    ])
+                elif row[currentitem + 2] == 'Slot B':
+                    slotB.append([
+                        row[currentitem],
+                        anime['data']['url'],
+                        str(row[currentitem + 3]).replace("'", "\'"),
+                        anime['data']['images']['jpg']['image_url'],
+                        anime['data']['synopsis'],
+                        anime['data']['trailer']['url']
+                    ])
+                elif row[currentitem + 2] == 'Slot C':
+                    slotC.append([
+                        row[currentitem],
+                        anime['data']['url'],
+                        str(row[currentitem + 3]).replace("'", "\'"),
+                        anime['data']['images']['jpg']['image_url'],
+                        anime['data']['synopsis'],
+                        anime['data']['trailer']['url']
+                    ])
+                time.sleep(1)
+                currentitem += 4
 
-with open('Slot B.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
-    for row in slotB:
-        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
+    with open('Slot A.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',')
+        filewriter.writerow(['Title', 'MAL Link', 'Nomination Description', 'Image URL', 'Synopsis', 'YouTube URL'])
+        for row in slotA:
+            try:
+                filewriter.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+            except UnicodeError:
+                filewriter.writerow([row[0], row[1], "Ahhhhh", row[3], row[4], row[5]])
 
-with open('Slot C.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile,delimiter=',')
-    filewriter.writerow(['Title','MAL Link','Nomination Description','Image URL','Synopsis'])
-    for row in slotC:
-        filewriter.writerow([row[0],row[1],row[2],row[3],row[4]])
+    with open('Slot B.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',')
+        filewriter.writerow(['Title', 'MAL Link', 'Nomination Description', 'Image URL', 'Synopsis'])
+        for row in slotB:
+            try:
+                filewriter.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+            except UnicodeError:
+                filewriter.writerow([row[0], row[1], "Ahhhhh", row[3], row[4], row[5]])
 
-print("Complete")
+    with open('Slot C.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',')
+        filewriter.writerow(['Title', 'MAL Link', 'Nomination Description', 'Image URL', 'Synopsis'])
+        for row in slotC:
+            try:
+                filewriter.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+            except UnicodeError:
+                filewriter.writerow([row[0], row[1], "Ahhhhh", row[3], row[4], row[5]])
+
+    print("Complete")
+
+
+if __name__ == '__main__':
+    main()
